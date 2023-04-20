75e0a4e4d6694a3e0cda25009b9ba9c6813676e2
plinsy <plinsy2@gmail.com>
2023-04-20 12:06:33 +0300

    Three store concepts implemented

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("From index.js");
+const redux = require("redux");
+
+const BUY_CAKE = "BUY_CAKE";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
+
+// (previousState, action) => newState
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = redux.createStore(reducer);
+// const store = configureStore(reducer)
+
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+unsubscribe();
